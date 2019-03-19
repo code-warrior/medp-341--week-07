6623b45df0131320a4e1c21819c901a19f80af0c
Roy A Vanegas <roy@thecodeeducators.com>
2019-03-19 11:31:11 -0400

    Add example of DOM selection

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 window.onload = () => {
+    // let body = document.getElementsByTagName();
+
+    let firstDiv = document.getElementById(`first-div`);
+    let secondDiv = document.querySelector(`[data-second-div]`);
+    let thirdDiv = document.getElementsByClassName(`third-div`)[0];
+
+    console.log(firstDiv.textContent);
+    console.log(secondDiv.textContent);
+    console.log(thirdDiv.textContent);
 };
