0a4b6819daffe6859b69b6b6473a9f1d5ac27b9a
Roy Vanegas <roy@thecodeeducators.com>
2019-03-20 13:36:28 -0400

    Add a variable that will be used to style output in the console

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 window.onload = () => {
     let firstDiv = document.getElementById(`first-div`),
         secondDiv = document.querySelector(`[data-second-div]`),
-        thirdDiv = document.getElementsByClassName(`third-div`)[0];
+        thirdDiv = document.getElementsByClassName(`third-div`)[0],
+        css = `background-color: #bada55; border-radius: 4px; padding: 4px;`;
 
     console.log(firstDiv.textContent);
     console.log(secondDiv.textContent);
