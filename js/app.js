e893e39a3e695232828f6ad785d4a1c2ec5381b3
Roy Vanegas <roy@thecodeeducators.com>
2019-03-20 13:54:46 -0400

    Add example of styling console.warn and console.error

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 window.onload = () => {
+    let cssWarning =
+            `color: black;` +
+            `background-color: #fabd32;` +
+            `border-radius: 4px;` +
+            `padding: 4px;`,
+        cssError =
+            `color: black;` +
+            `background-color: #f53846;` +
+            `border-radius: 4px;` +
+            `padding: 4px;`;
 
+    console.warn(`%c console.warn `, cssWarning);
+    console.error(`%c console.error `, cssError);
 };
